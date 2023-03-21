ceb9569eb81ac12e6ace5bddf4edd07da1aebeed
srwing <694177407@qq.com>
2023-03-21 17:15:09 +0800

    开放rx的依赖

--- app/src/main/java/com/example/gxylib/main/v/TestActivity2.kt
+++ app/src/main/java/com/example/gxylib/main/v/TestActivity2.kt
@@ -1,37 +1,37 @@
-//package com.example.gxylib.main.v
-//
-//import android.content.Intent
-//import android.view.View
-//import androidx.appcompat.widget.AppCompatTextView
-//import com.example.gxylib.R
-//import com.example.gxylib.databinding.ActivityMainBinding
-//import com.example.gxylib.main.base.BindingActivity
-//import com.srwing.b_applib.launch.GxyLauncher
-//import com.srwing.b_applib.launch.IGxyLauncher
-//import com.srwing.t_network.utils.GxyLogger
-//
-///**
-// * Description:kotlin页面中通过委托 来实现startActivityForResult
-// * Created srwing
-// * Date: 2023/2/9
-// * Email: 694177407@qq.com
-// */
-//class TestActivity2 : BindingActivity<ActivityMainBinding?>(), IGxyLauncher by GxyLauncher() {
-//    override fun getContentView(): Int {
-//        return R.layout.activity_main
-//    }
-//    override fun getTitleLayout(): Int {
-//        return R.layout.b_base_title_bar
-//    }
-//
-//    override fun setTitleContent(view: View) {
-//        // 添加标题的一些事件
-//        val tv = view.findViewById<AppCompatTextView>(R.id.titleText)
-//        tv.text = "页面2"
-//    }
-//    override fun initViewData() {
-//        super.initViewData()
-//        initLauncher()
+package com.example.gxylib.main.v
+
+import android.content.Intent
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import com.example.gxylib.R
+import com.example.gxylib.databinding.ActivityMainBinding
+import com.example.gxylib.main.base.BindingActivity
+import com.srwing.b_applib.launch.GxyLauncher
+import com.srwing.b_applib.launch.IGxyLauncher
+import com.srwing.t_network.utils.GxyLogger
+
+/**
+ * Description:kotlin页面中通过委托 来实现startActivityForResult
+ * Created srwing
+ * Date: 2023/2/9
+ * Email: 694177407@qq.com
+ */
+class TestActivity2 : BindingActivity<ActivityMainBinding?>(), IGxyLauncher by GxyLauncher() {
+    override fun getContentView(): Int {
+        return R.layout.activity_main
+    }
+    override fun getTitleLayout(): Int {
+        return R.layout.b_base_title_bar
+    }
+
+    override fun setTitleContent(view: View) {
+        // 添加标题的一些事件
+        val tv = view.findViewById<AppCompatTextView>(R.id.titleText)
+        tv.text = "页面2"
+    }
+    override fun initViewData() {
+        super.initViewData()
+        initLauncher()
 //        dataBinding!!.tvText.text = "这是页面2 data: " + intent.getExtras()?.get("data")
 //        dataBinding!!.tvText.setOnClickListener {
 //            val intent = Intent()
@@ -47,5 +47,5 @@
 //                            it.getData()?.getStringExtra("data"))
 //            }
 //        }
-//    }
-//}
+    }
+}
